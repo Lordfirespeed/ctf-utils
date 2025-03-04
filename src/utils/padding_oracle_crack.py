a36fc22cc6723b3175c753b1f15a1c913075b5f3
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-04 17:06:15 +0000

    refactor to build plaintext on-the-fly

--- src/utils/padding_oracle_crack.py
+++ src/utils/padding_oracle_crack.py
@@ -3,10 +3,20 @@ from typing import Callable, ClassVar, Generator
 type PaddingOracle = Callable[[bytes], bool]
 
 
-class PartialBlock:
-    def __init__(self, block_length: int) -> None:
+class PartiallyCrackedBlock:
+    def __init__(self,
+        block_length: int,
+        preceding_ciphertext_block: bytes,
+        ciphertext_block: bytes,
+    ) -> None:
+        assert len(preceding_ciphertext_block) == block_length
+        assert len(ciphertext_block) == block_length
+
         self.block_length = block_length
-        self._known_bytes = bytearray(block_length)
+        self.preceding_ciphertext = preceding_ciphertext_block 
+        self.ciphertext = ciphertext_block
+        self._known_decryption = bytearray(block_length)
+        self._known_plaintext = bytearray(block_length)
         self.num_bytes_known = 0
 
     @property
@@ -14,18 +24,25 @@ class PartialBlock:
         return self.block_length - self.num_bytes_known
 
     @property
-    def known_bytes(self) -> bytes:
-        return bytes(self._known_bytes)
+    def known_decryption(self) -> bytes:
+        return bytes(self._known_decryption)
+
+    @property
+    def known_plaintext(self) -> bytes:
+        return bytes(self._known_plaintext)
 
     def is_complete(self) -> bool:
         return self.num_bytes_known == self.block_length
 
-    def update(self, new_known_bytes: bytes) -> None:
-        assert len(new_known_bytes) <= self.num_bytes_unknown
-        old_first_known_byte_index = self.num_bytes_unknown
-        new_first_known_byte_index = old_first_known_byte_index - len(new_known_bytes)
-        self._known_bytes[new_first_known_byte_index:old_first_known_byte_index] = new_known_bytes
-        self.num_bytes_known += len(new_known_bytes)
+    def update(self, new_known_byte_decryption: int) -> None:
+        assert 0 <= new_known_byte_decryption < 256
+
+        first_known_byte_index = self.num_bytes_unknown
+        last_unknown_byte_index = first_known_byte_index - 1
+        self._known_decryption[last_unknown_byte_index] = new_known_byte_decryption
+        mask_byte = self.preceding_ciphertext[last_unknown_byte_index]
+        self._known_plaintext[last_unknown_byte_index] = new_known_byte_decryption ^ mask_byte
+        self.num_bytes_known += 1
 
 
 class PaddingOracleCracker:
@@ -84,42 +101,30 @@ class PaddingOracleCracker:
         return plaintext
 
     def crack_plaintext_block(self, plaintext_block_index: int) -> bytes:
-        partial_decryption = PartialBlock(self.block_length)
+        partial_decryption = PartiallyCrackedBlock(
+            self.block_length,
+            self._block_at(plaintext_block_index),
+            self._ciphertext_block_at(plaintext_block_index),
+        )
         while not partial_decryption.is_complete():
-            self.step_crack_plaintext_block(
-                plaintext_block_index,
-                partial_decryption,
-            )
-            print(partial_decryption.known_bytes)
+            self.step_crack_plaintext_block(partial_decryption)
+            print(partial_decryption.known_plaintext)
 
-        preceding_block = self._block_at(plaintext_block_index)
-        plaintext = bytearray(partial_decryption.known_bytes)
-        for byte_index in range(self.block_length):
-            plaintext[byte_index] ^= preceding_block[byte_index]
-        return plaintext
+        return partial_decryption.known_plaintext
 
-    def step_crack_plaintext_block(
-        self,
-        plaintext_block_index: int,
-        partial_decryption: PartialBlock,
-    ) -> None:
-        assert partial_decryption.block_length == self.block_length
+    def step_crack_plaintext_block(self, partial_decryption: PartiallyCrackedBlock) -> None:
         assert not partial_decryption.is_complete()
 
         first_known_byte_index = partial_decryption.num_bytes_unknown
         last_unknown_byte_index = first_known_byte_index - 1
 
-        preceding_block = bytearray(self._block_at(plaintext_block_index))
-        other_preceding_block = bytearray(self._block_at(plaintext_block_index))
-        ciphertext_block = self._ciphertext_block_at(plaintext_block_index)
+        preceding_block = bytearray(partial_decryption.preceding_ciphertext)
+        ciphertext_block = partial_decryption.ciphertext
 
         # spoof padding for known bytes
         target_padding = partial_decryption.num_bytes_known + 1
         for byte_index in range(first_known_byte_index, self.block_length):
-            preceding_block[byte_index] = partial_decryption.known_bytes[byte_index] ^ target_padding
-            other_preceding_block[byte_index] = partial_decryption.known_bytes[byte_index] ^ (target_padding - 1)
-
-        # assert target_padding == 1 or self.oracle(other_preceding_block + ciphertext_block)
+            preceding_block[byte_index] = partial_decryption.known_decryption[byte_index] ^ target_padding
 
         # find a mutation which turns the last unknown byte into valid padding
         for try_byte_value in range(256):
@@ -138,4 +143,4 @@ class PaddingOracleCracker:
         else:
             raise Exception("no value found")
 
-        partial_decryption.update((c1_prime[last_unknown_byte_index] ^ target_padding).to_bytes(length=1))
+        partial_decryption.update(c1_prime[last_unknown_byte_index] ^ target_padding)
