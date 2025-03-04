e5e715bf48e35389ea964b5ee630b1af7213d9bb
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-04 20:20:00 +0000

    add type hints + run two servers concurrently

--- src/examples/sockets/echo_server.py
+++ src/examples/sockets/echo_server.py
@@ -1,12 +1,12 @@
 import asyncio
+from asyncio import StreamReader, StreamWriter
 from pathlib import Path
 
 
 # https://docs.python.org/3/library/asyncio-stream.html#tcp-echo-server-using-streams
-async def handle_echo(reader, writer):
+async def handle_echo(reader: StreamReader, writer: StreamWriter) -> None:
     data = await reader.read(100)
     message = data.decode()
-
     print(f"Received {message!r}")
 
     print(f"Send: {message!r}")
@@ -18,14 +18,31 @@ async def handle_echo(reader, writer):
     await writer.wait_closed()
 
 
-async def main():
-    server = await asyncio.start_unix_server(handle_echo, Path("/", "tmp", "echo-server.sock"))
+async def handle_echo_uppercase(reader: StreamReader, writer: StreamWriter) -> None:
+    data = await reader.read(100)
+    message = data.decode()
+    print(f"Received {message!r}")
+    
+    response = message.upper()
+    print(f"Send: {response!r}")
+    writer.write(response.encode())
+    await writer.drain()
 
-    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print("Close the connection")
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    echo_server = await asyncio.start_unix_server(handle_echo, Path("/", "tmp", "echo-server.sock"))
+    echo_uppercase_server = await asyncio.start_unix_server(handle_echo_uppercase, Path("/", "tmp", "echo-uppercase-server.sock"))
+
+    addrs = ', '.join(str(sock.getsockname()) for server in (echo_server, echo_uppercase_server) for sock in server.sockets)
     print(f'Serving on {addrs}')
 
-    async with server:
-        await server.serve_forever()
+    async with echo_server, echo_uppercase_server:
+        await echo_server.serve_forever()
+        await echo_uppercase_server.serve_forever()
 
 
 if __name__ == "__main__":
