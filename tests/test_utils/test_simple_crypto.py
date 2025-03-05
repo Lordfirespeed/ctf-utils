122fefb0f34ac12c73ca66a65a58687a9c60d5fc
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-05 15:04:35 +0000

    split simple crypto tests out from padding oracle cracker tests

--- tests/test_utils/test_simple_crypto.py
+++ tests/test_utils/test_simple_crypto.py
@@ -1,19 +1,11 @@
 from secrets import token_urlsafe
 import unittest
 
-from utils.padding_oracle_crack import PaddingOracleCracker
 from utils.simple_crypto import SimpleCrypto
 
 
-class PaddingOracleCrackerTests(unittest.TestCase):
+class SimpleCryptoTests(unittest.TestCase):
     crypto: SimpleCrypto
-
-    def oracle(self, iv: bytes, ciphertext: bytes) -> bool:
-        try:
-            self.crypto.decrypt(iv, ciphertext)
-        except (ValueError, AssertionError):
-            return False
-        return True
 
     def setUp(self):
         self.crypto = SimpleCrypto()
@@ -21,12 +13,11 @@ class PaddingOracleCrackerTests(unittest.TestCase):
     def tearDown(self):
         self.crypto = None
 
-    def test_crack(self):
+    def test_crypto(self):
         message = token_urlsafe()
         plaintext = message.encode("utf-8")
         iv, ciphertext = self.crypto.encrypt(plaintext)
-        cracker = PaddingOracleCracker(iv, ciphertext, self.oracle, render_progress=False)
-        round_trip_plaintext = cracker.crack_plaintext()
+        round_trip_plaintext = self.crypto.decrypt(iv, ciphertext)
         round_trip_message = round_trip_plaintext.decode("utf-8")
         self.assertEqual(message, round_trip_message)
 
