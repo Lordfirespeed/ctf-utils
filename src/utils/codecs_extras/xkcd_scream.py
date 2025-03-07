f871ed50caf12fcc7ea4c26ea359211c2ec181ca
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-07 00:55:20 +0000

    add `xkcd_scream` module

---
+++ src/utils/codecs_extras/xkcd_scream.py
@@ -0,0 +1,96 @@
+import codecs
+from string import ascii_lowercase, ascii_uppercase
+from unicodedata import normalize as normalise
+
+from ugrapheme import graphemes
+
+ascii_lowercase_b_thru_y = ascii_lowercase[1:-1]
+ascii_uppercase_b_thru_y = ascii_uppercase[1:-1]
+
+accents_for_b_thru_y = (
+    (u"\u0307",),  # B
+    (u"\u0327",),  # C
+    (u"\u0331", u"\u0332"),  # D
+    (u"\u0301",),  # E
+    (u"\u032e",),  # F
+    (u"\u030b",),  # G
+    (u"\u0330",),  # H
+    (u"\u0309",),  # I
+    (u"\u0313",),  # J
+    (u"\u0323",),  # K
+    (u"\u0306",),  # L
+    (u"\u030c",),  # M
+    (u"\u0302",),  # N
+    (u"\u030a",),  # O
+    (u"\u032f",),  # P
+    (u"\u0324",),  # Q
+    (u"\u0311",),  # R
+    (u"\u0303",),  # S
+    (u"\u0304",),  # T
+    (u"\u0308",),  # U
+    (u"\u0300",),  # V
+    (u"\u030f",),  # W
+    (u"\u033d",),  # X
+    (u"\u0326",),  # Y
+)
+
+
+
+character_relation = (
+    ("A", "A"),
+    *((character, f"A{accent}") for character, accents in zip(ascii_uppercase_b_thru_y, accents_for_b_thru_y) for accent in accents),
+    ("Z", u"\u023a"),
+    ("a", "a"),
+    *((character, f"a{accent}") for character, accents in zip(ascii_lowercase_b_thru_y, accents_for_b_thru_y) for accent in accents),
+    ("z", u"\u2c65"),
+)
+
+normalised_character_relation = tuple(
+    (ascii_character, normalise("NFC", scream_assignment)) for ascii_character, scream_assignment in character_relation
+)
+normalised_character_relation_inverse = (
+    (scream_assignment, ascii_character) for ascii_character, scream_assignment in normalised_character_relation
+)
+
+screamify_lookup = dict(normalised_character_relation)
+unscreamify_lookup = dict(normalised_character_relation_inverse)
+
+
+def screamify(value: str) -> str:
+    input_bytes = bytearray(value, "ascii")
+    cursor = 0
+    for character in value:
+        scream_assignment = screamify_lookup.get(character, None)
+        if scream_assignment is None:
+            cursor += 1
+            continue
+        scream_assignment_bytes = scream_assignment.encode("utf-8")
+        input_bytes[cursor:cursor+1] = scream_assignment_bytes
+        cursor += len(scream_assignment_bytes)
+    return input_bytes.decode("utf-8")
+
+
+def unscreamify(value: str) -> str:
+    input_bytes = bytearray(value, "utf-8")
+    cursor = 0
+    for grapheme in graphemes(value):
+        grapheme_byte_length = len(grapheme.encode("utf-8"))
+        ascii_character = unscreamify_lookup.get(grapheme, None)
+        if ascii_character is None:
+            cursor += grapheme_byte_length
+            continue
+        ascii_character_bytes = ascii_character.encode("ascii")
+        input_bytes[cursor:cursor+grapheme_byte_length] = ascii_character_bytes
+        cursor += 1
+    return input_bytes.decode("ascii")
+
+
+class XKCDScreamCodec(codecs.Codec):
+    def encode(self, value: str, errors ="strict") -> str:
+        return screamify(value)
+
+    def decode(self, value: str, errors ="strict") -> str:
+        return unscreamify(value)
+
+
+__all__ = ("XKCDScreamCodec", "screamify", "unscreamify",)
---
+++ tests/test_utils/test_codecs_extras/test_xkcd_scream.py
@@ -0,0 +1,30 @@
+import unittest
+
+from numpy.ma.core import round_
+
+from utils.codecs_extras.xkcd_scream import screamify, unscreamify
+
+
+class XKCDScreamTests(unittest.TestCase):
+    def test_letter_d(self) -> None:
+        """
+        Some 'scream' implementations consider the encryption of `D` to be `A\u0331`, others
+        consider it to be `A\u0332`.
+        `unscreamify` should successfully decrypt either.
+        """
+        first_d = unscreamify("A\u0331a\u0331")
+        second_d = unscreamify("A\u0332a\u0332")
+        self.assertEqual(first_d, second_d)
+
+    def test_flag_style_vector(self) -> None:
+        ciphertext = u"a̮ăaa̋{â_A̮âA̧a̤A̦a̽A̦ȃȁA̮ȃȁA̮a̦A̰AÄå_19ăȁa̦Äa̯_âȺáA̯äāâÂa̽A̽Ȃ-ⱥã}"
+        plaintext = unscreamify(ciphertext)
+        self.assertEqual(plaintext, "flag{n_FnCqYxYrwFrwFyHAUo_19lwyUp_nZePutnNxXR-zs}")
+        round_trip_ciphertext = screamify(plaintext)
+        self.assertEqual(round_trip_ciphertext, ciphertext)
+        round_trip_plaintext = unscreamify(round_trip_ciphertext)
+        self.assertEqual(round_trip_plaintext, plaintext)
+
+
+if __name__ == "__main__":
+    unittest.main()
