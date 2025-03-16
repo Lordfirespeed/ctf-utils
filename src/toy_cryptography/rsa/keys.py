d434b9057ea081e095371b275599e73938ef6df7
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-16 01:21:41 +0000

    create `toy_cryptography.rsa` module

---
+++ src/toy_cryptography/rsa/keys.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass, field
+from math import gcd
+
+from extras.random_extras import prime_randint_of_digit_length, randint_coprime_to
+
+
+@dataclass
+class RSAPublicKey:
+    modulus: int
+    public_exponent: int
+
+
+@dataclass
+class RSAPrivateKey:
+    # stored values absolutely necessary for any implementation
+    modulus: int  # 'n = pq'
+    public_exponent: int  # 'e'
+    private_exponent: int  # 'd = e^{-1} mod phi(n)'
+
+    # stored values only used for more efficient implementation & integrity checks
+    # see https://en.wikipedia.org/wiki/RSA_(cryptosystem)#Using_the_Chinese_remainder_algorithm
+    prime1: int = field(repr=False)  # 'p'
+    prime2: int = field(repr=False)  # 'q'
+    private_exponent1: int = field(repr=False)  # 'd_p = d mod (p-1)'
+    private_exponent2: int = field(repr=False)  # 'd_q = d mod (q-1)'
+    coefficient: int = field(repr=False)  # 'q_{inv} = q^{-1} mod p`
+
+    @property
+    def group_order(self) -> int:
+        return (self.prime1 - 1) * (self.prime2 - 1)
+
+    def __post_init__(self):
+        assert self.modulus == self.prime1 * self.prime2, \
+            "modulus should be product of prime1 and prime2"
+        assert gcd(self.public_exponent, self.group_order) == 1, \
+            "public_exponent should be co-prime to phi(modulus)"
+        assert (self.public_exponent * self.private_exponent) % self.group_order == 1, \
+            "private_exponent should be public_exponent's mul. inverse mod phi(modulus)"
+        assert self.private_exponent1 == self.private_exponent % (self.prime1 - 1), \
+            "private_exponent1 should be private_exponent mod (prime1 -1 )"
+        assert self.private_exponent2 == self.private_exponent % (self.prime2 - 1), \
+            "private_exponent2 should be private_exponent mod (prime2 - 1)"
+        assert (self.prime2 * self.coefficient) % self.prime1 == 1, \
+            "coefficient should be prime2's mul. inverse mod prime1"
+
+    def extract_public_key(self) -> RSAPublicKey:
+        return RSAPublicKey(
+            modulus=self.modulus,
+            public_exponent=self.public_exponent,
+        )
+
+
+def determine_private_key(
+    prime1: int,  # p
+    prime2: int,  # q
+    public_exponent: int,  # e
+) -> RSAPrivateKey:
+    modulus = prime1 * prime2
+    group_order = (prime1 - 1) * (prime2 - 1)  # phi(modulus)
+    private_exponent = pow(public_exponent, -1, mod=group_order)
+
+    private_exponent1 = private_exponent % (prime1 - 1)
+    private_exponent2 = private_exponent % (prime2 - 1)
+    coefficient = pow(prime2, -1, mod=prime1)
+
+    return RSAPrivateKey(
+        modulus=modulus,
+        public_exponent=public_exponent,
+        private_exponent=private_exponent,
+
+        prime1=prime1,
+        prime2=prime2,
+        private_exponent1=private_exponent1,
+        private_exponent2=private_exponent2,
+        coefficient=coefficient,
+    )
+
+
+def gen_private_key(prime_digit_length: int = 12) -> RSAPrivateKey:
+    prime1 = prime_randint_of_digit_length(prime_digit_length)
+    prime2 = prime_randint_of_digit_length(prime_digit_length)
+    group_order = (prime1 - 1) * (prime2 - 1)
+    public_exponent = randint_coprime_to(group_order)
+
+    return determine_private_key(prime1, prime2, public_exponent)
+
+
+__all__ = ("RSAPublicKey", "RSAPrivateKey", "determine_private_key", "gen_private_key",)
---
+++ src/toy_cryptography/rsa/load_key.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_private_key as parse_pem_private_key,
+)
+from cryptography.hazmat.primitives.asymmetric import rsa as pyca_rsa
+
+from .keys import RSAPrivateKey
+
+
+def load_pem_rsa_private_key(parameters_file: Path) -> RSAPrivateKey:
+    """
+    ```bash
+    openssl genpkey -algorithm rsa -out key.pem
+    ```
+    """
+    with open(parameters_file, "rb") as parameters_file_handle:
+        data = parameters_file_handle.read()
+    parsed_key = parse_pem_private_key(data, None)
+    assert isinstance(parsed_key, pyca_rsa.RSAPrivateKey)
+    private_numbers = parsed_key.private_numbers()
+    public_numbers = private_numbers.public_numbers
+    return RSAPrivateKey(
+        modulus=public_numbers.n,
+        public_exponent=public_numbers.e,
+        private_exponent=private_numbers.d,
+
+        prime1=private_numbers.p,
+        prime2=private_numbers.q,
+        private_exponent1=private_numbers.dmp1,
+        private_exponent2=private_numbers.dmq1,
+        coefficient=private_numbers.iqmp,
+    )
+
+
+__all__ = ("load_pem_rsa_private_key",)
