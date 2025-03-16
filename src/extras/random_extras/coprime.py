e2011022cbc2d88ca0cf21eafb4a0cc5fb417f6d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-16 00:52:00 +0000

    fix: `randint_coprime_to` implementation was not correct

--- src/extras/random_extras/coprime.py
+++ src/extras/random_extras/coprime.py
@@ -1,19 +1,36 @@
 from extras.math_extras import extended_euclidian_algorithm
-from extras.random_extras.sysrandom import getrandbits
+from extras.random_extras.sysrandom import randbelow
 
 
 def randint_coprime_to(value: int) -> int:
     """Return a random integer (non-trivially) co-prime to `value`."""
+    if value <= 2:
+        # reject negative values
+        # 0's unique positive co-prime value is 1, but 1 is trivial, so reject
+        # similar argument for 1, 2
+        raise ValueError
+
+    def make_coprime(trial_value: int) -> int:
+        """Return an integer co-prime to `value` which may be trivial, depending on `trial_value`"""
+        while True:
+            if trial_value == 1:
+                break
+
+            gcd = extended_euclidian_algorithm(trial_value, value).gcd
+            if gcd == 1:
+                break
+
+            trial_value //= gcd
+        return trial_value
+
     while True:
-        candidate = getrandbits(value.bit_length())
+        candidate = randbelow(value)
         if candidate == 0:
             continue
-        gcd = extended_euclidian_algorithm(candidate, value).gcd
-        if gcd != candidate:
+        candidate = make_coprime(candidate)
+        if candidate != 1:
             break
 
-    if gcd > 1:
-        return candidate // gcd
     return candidate
 
 
