41ae621991dc70976faf70c4efcf831a1c298a36
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-18 14:10:53 +0000

    add Eratosthenes' sieve implementation

---
+++ src/extras/math_extras/prime_sieve.py
@@ -0,0 +1,24 @@
+from utils.bit_set import BitSet
+
+
+def sieve_primes_less_than(bound: int) -> list[int]:
+    primes = []
+    sieve = BitSet(bound)
+    sieve[0] = True
+    prime = 1
+
+    while True:
+        prime = sieve.next_clear_bit_index(prime - 1) + 1
+        if prime >= bound:
+            break
+        primes.append(prime)
+
+        cursor = prime - 1
+        while cursor < bound:
+            sieve[cursor] = True
+            cursor += prime
+
+    return primes
+
+
+__all__ = ("sieve_primes_less_than",)
