a5cca1b39cf5533b3ed7149a86f2011d4b212982
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-21 18:01:39 +0000

    add `importlib_extras` module

---
+++ src/extras/importlib_extras/__init__.py
@@ -0,0 +1,1 @@
+from .import_all_names import *
---
+++ src/extras/importlib_extras/import_all_names.py
@@ -0,0 +1,35 @@
+import importlib
+from typing import Any
+
+
+class UniqueType(object):
+    def __repr__(self):
+        return "Unique"
+
+
+Unique = UniqueType()
+
+
+def import_all_names_from(_locals: dict[str, Any], name: str, package: str = None) -> None:
+    """Import all names from a module.
+
+    The 'package' argument is required when performing a relative import. It
+    specifies the package to use as the anchor point from which to resolve the
+    relative import to an absolute import.
+    """
+
+    module = importlib.import_module(name, package)
+    if not hasattr(module, "__all__"):
+        raise NotImplementedError(f"caller tried to import all names from {module.__package__}, but that module has no `__all__` variable")
+
+    for name in module.__all__:
+        member = getattr(module, name)
+        masked = _locals.get(name, Unique)
+        if masked is member:
+            continue
+        if masked is not Unique:
+            raise ValueError(f"caller tried to import {name} from {module.__package__} but doing so will mask {masked} in caller's scope")
+        _locals[name] = member
+
+
+__all__ = ("import_all_names_from",)
