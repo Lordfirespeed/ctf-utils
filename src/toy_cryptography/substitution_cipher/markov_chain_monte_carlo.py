4068b51267b227b27e43e0b79752ffee736f079b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-03 16:44:48 +0100

    significantly improve success rate plausibility maximiser by taking 'best' steps

--- src/toy_cryptography/substitution_cipher/markov_chain_monte_carlo.py
+++ src/toy_cryptography/substitution_cipher/markov_chain_monte_carlo.py
@@ -2,9 +2,10 @@
 https://www.ams.org/journals/bull/2009-46-02/S0273-0979-08-01238-X/S0273-0979-08-01238-X.pdf
 """
 import asyncio
+from contextlib import ExitStack, suppress
 import itertools
 from string import ascii_lowercase
-from typing import Callable, NewType
+from typing import Callable, Iterable, NewType
 
 from gmpy2 import mpq
 
@@ -18,6 +19,8 @@ from .scheme import CipherKey, decode
 
 BigramProportions = NewType("BigramProportions", sortabledict[str, mpq])
 Plausibility = NewType("Plausibility", mpq)
+type Swap = (str, str)
+type MutableCipherKey = bidict[str, str]
 
 
 async def load_english_bigram_proportions() -> BigramProportions:
@@ -50,10 +53,6 @@ def bigram_plausibility(plaintext: str, reference: BigramProportions = None) -> 
 
 
 class PlaintextPlausibilityMaximiser:
-
-    type Swap = (str, str)
-    type MutableCipherKey = bidict[str, str]
-
     def __init__(
         self,
         ciphertext: str,
@@ -72,8 +71,10 @@ class PlaintextPlausibilityMaximiser:
         return sysrandom.sample(self.plaintext_alphabet, 2)
 
     @staticmethod
-    def apply_swap(swap: Swap, key: MutableCipherKey):
+    def apply_swap(swap: Swap | None, key: MutableCipherKey):
         """Note that applying a swap for a second time is functionally equivalent to 'undoing' the swap."""
+        if swap is None:
+            return
         first, second = swap
         first_image = key[first]
         second_image = key[second]
@@ -88,37 +89,74 @@ class PlaintextPlausibilityMaximiser:
         plaintext_preview = decode(self.ciphertext[:25], self.current_key)
         printer(f"epoch {iteration:5}: '{plaintext_preview}...'")
 
-    def step(self) -> None:
+    def step(self) -> bool:
         swap = self.random_swap()
         self.apply_swap(swap, self.current_key)
         new_plausibility = self.compute_plausibility(self.current_key)
 
-        def accept() -> None:
+        def accept() -> bool:
             self.current_plausibility = new_plausibility
             self.steps += 1
             self.steps_without_acceptance = 0
+            return True
 
-        def reject() -> None:
+        def reject() -> bool:
             self.apply_swap(swap, self.current_key)
             self.steps += 1
             self.steps_without_acceptance += 1
+            return False
 
         if new_plausibility > self.current_plausibility:
-            accept()
-            return
+            return accept()
         if sysrandom.coin_flip(p=(new_plausibility / self.current_plausibility)):
-            accept()
-            return
+            return accept()
 
-        reject()
+        return reject()
 
-    def main_loop(self, printer: Callable[[str], None], threshold: int = 350) -> None:
+    def compute_best_swap(self):
+        def swap_metric(swap: Swap | None) -> mpq:
+            self.apply_swap(swap, self.current_key)
+            plausibility = self.compute_plausibility(self.current_key)
+            self.apply_swap(swap, self.current_key)
+            return plausibility
+
+        all_swaps: Iterable[Swap] = itertools.combinations(ascii_lowercase, 2)
+        all_swaps = itertools.chain((None, ), all_swaps)
+        return max(all_swaps, key=swap_metric)
+
+    def best_step(self) -> bool:
+        best_swap = self.compute_best_swap()
+        if best_swap is None:
+            self.steps_without_acceptance += 1
+            self.steps += 1
+            return False
+
+        self.apply_swap(best_swap, self.current_key)
+        self.steps_without_acceptance = 0
+        self.steps += 1
+        return True
+
+    def many_best_step(self, *, count: int = 5) -> None:
+        target = self.steps + count
+        while self.steps < target:
+            self.best_step()
+
+    def _main_loop(self, printer: Callable[[str], None], threshold: int) -> None:
+        self.many_best_step(count=5)
         while True:
             if self.steps % 100 == 0:
                 self.print_status(self.steps, printer)
             if self.steps_without_acceptance >= threshold:
-                break
+                accepted_swap = self.best_step()
+                if not accepted_swap: break
+
             self.step()
+
+    def main_loop(self, threshold: int = 350) -> None:
+        with ExitStack() as stack:
+            printer = stack.enter_context(Printer())
+            stack.enter_context(suppress(KeyboardInterrupt))
+            self._main_loop(printer, threshold)
 
     def many_steps(self, *, count: int) -> None:
         target = self.steps + count
@@ -126,33 +164,16 @@ class PlaintextPlausibilityMaximiser:
             self.step()
 
 
-def maximise_plaintext_plausibility(
-    ciphertext: str,
-    initial_key: CipherKey,
-    reference: BigramProportions = None,
-) -> CipherKey:
-    maximiser = PlaintextPlausibilityMaximiser(
-        ciphertext,
-        initial_key,
-        reference,
-    )
-    with Printer() as printer:
-        try:
-            maximiser.main_loop(printer)
-        except KeyboardInterrupt:
-            pass
-    return maximiser.current_key
-
-
-__all__ = ("BigramProportions", "Plausibility", "bigram_plausibility", "maximise_plaintext_plausibility",)
+__all__ = ("BigramProportions", "Plausibility", "bigram_plausibility", "PlaintextPlausibilityMaximiser",)
 
 
 def main():
     from .analyser import ciphertext, analyse_character_proportions, infer_cipher_key
     ciphertext_character_frequency = analyse_character_proportions(ciphertext)
     cipher_key = infer_cipher_key(ciphertext_character_frequency)
-    better_key = maximise_plaintext_plausibility(ciphertext, cipher_key)
-    print(better_key)
+    chain = PlaintextPlausibilityMaximiser(ciphertext, cipher_key)
+    chain.main_loop()
+    better_key = chain.current_key
     print("".join(better_key[c] for c in ascii_lowercase))
 
 
