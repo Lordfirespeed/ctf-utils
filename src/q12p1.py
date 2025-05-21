f17dd579e66ea3b07ce9e8aa31820075962e7b74
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-21 04:06:48 +0100

    tighten up table representation

--- src/q12p1.py
+++ src/q12p1.py
@@ -10,9 +10,9 @@ def main():
     generated_points = list(curve.scalar_mul_affine_point(k, generator) for k in range(len(points)))
     assert points == set(generated_points)
 
-    print(f"{'':>25}" + "".join(f"{point!s:^25}" for point in generated_points))
+    print(f"{'':>12}" + "".join(f"{point!s:^12}" for point in generated_points))
     for point_1 in generated_points:
-        print(f"{point_1!s:>25}" + "".join(f"{curve.add_affine_points(point_1, point_2)!s:^25}" for point_2 in generated_points))
+        print(f"{point_1!s:^12}" + "".join(f"{curve.add_affine_points(point_1, point_2)!s:^12}" for point_2 in generated_points))
 
 
 if __name__ == "__main__":
