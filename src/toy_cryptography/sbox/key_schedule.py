aa7c6ff58f562506c34de948325d6c266b5f389d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-12-04 17:12:44 +0000

    fix: K_0 is not used, first used round key is K_1

--- src/toy_cryptography/sbox/key_schedule.py
+++ src/toy_cryptography/sbox/key_schedule.py
@@ -8,8 +8,7 @@ from extras.binary_extras import circular_left_shift
 def sbox_key_schedule(key: bitarray) -> Generator[bitarray]:
     assert len(key) == 64
 
-    yield key[0:32]
-    for _ in range(5):
+    for _ in range(6):
         key = circular_left_shift(key, 33)
         yield key[0:32]
 
